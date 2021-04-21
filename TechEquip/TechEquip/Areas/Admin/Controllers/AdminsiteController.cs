07de10e252f2aa9db18b475eb3e5e5342e05a8ab
Snake <itquoctrung020601@gmail.com>
2021-04-21 14:18:56 +0700

    fix

--- TechEquip/TechEquip/Areas/Admin/Controllers/AdminsiteController.cs
+++ TechEquip/TechEquip/Areas/Admin/Controllers/AdminsiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechEquip.Models.ModelView;
 
 namespace TechEquip.Areas.Admin.Controllers
 {
@@ -15,11 +16,35 @@ namespace TechEquip.Areas.Admin.Controllers
         }
         public ActionResult Product()
         {
+            Models.Categoryne c =  new Models.Categoryne();
+            var q=c.GetAll();
+            ViewBag.list_category = q;
+
+            Models.Product_Bussiness pb = new Models.Product_Bussiness();
+            var q2 = pb.Getall();
+            ViewBag.list_product = q2;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create_Product(HttpPostedFileBase Img) {
+            ProductView pv = new ProductView();
+            pv.name_product = Request.Form["Name"];
+            pv.name_image = Img.FileName;
+            pv.description = Request.Form["Decription"];
+            pv.price = double.Parse(Request.Form["Price"]);
+            pv.id_category = int.Parse(Request.Form["Category_id"]);
+            pv.active = Request.Form["Active"].Equals("on") ? true : false;
+            string pathUpload = Server.MapPath("~/Areas/Upload/") + pv.name_image;
+            Img.SaveAs(pathUpload);
+            Models.Product_Bussiness pb = new Models.Product_Bussiness();
+            pb.Cretae(pv);
+            return RedirectToAction("Product");
         }
         public ActionResult Store()
         {
             return View();
         }
+                
     }
 }
---
+++ TechEquip/TechEquip/Models/Product_Bussiness.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using TechEquip.Models.Entity;
+using TechEquip.Models.ModelView;
+
+namespace TechEquip.Models
+{
+    public class Product_Bussiness
+    {
+        public int Cretae(ProductView item) {
+            try
+            {
+                Information_EquipmentEntities db = new Information_EquipmentEntities();
+                Product p = new Product() { name_product = item.name_product, name_image = item.name_image, description = item.description, price = item.price, id_category = item.id_category, active = item.active };
+                db.Products.Add(p);
+                db.SaveChanges();
+                return 1;
+            }
+            catch (EntityException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        public List<ProductView> Getall() { 
+                Information_EquipmentEntities db = new Information_EquipmentEntities();
+            var q = db.Products.Where(d => d.active == true).Select(d=>new ProductView {name_product=d.name_product,name_image=d.name_image,description=d.description,price=d.price??0,active=d.active??false,name_category=d.Category.name_category}).ToList();
+            return q;
+        }
+    }
+}
