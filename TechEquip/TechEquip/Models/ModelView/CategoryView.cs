3496c34d4b5b2294b5608b92f5c97ab2b24b0532
Snake <itquoctrung020601@gmail.com>
2021-04-21 15:43:05 +0700

    Product

---
+++ TechEquip/TechEquip/Models/ModelView/CategoryView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechEquip.Models.ModelView
+{
+    public class CategoryView
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool active { get; set; }
+    }
+
+}
