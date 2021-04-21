b3e00bde7e4e43ba77d385130898d3cec3ff3d7a
Hung <hung.vq98@gmail.com>
2021-04-21 14:03:42 +0700

    Header And Footer
    Big Update

---
+++ TechEquip/TechEquip/Areas/User/Controllers/UserSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechEquip.Areas.User.Controllers
+{
+    public class UserSiteController : Controller
+    {
+        // GET: User/UserSite
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
